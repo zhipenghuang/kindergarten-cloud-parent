fc70a637ea63f084c551df8de3bf3889c8c1ce3c
hzp <815152937@qq.com>
2018-09-28 00:26:25 +0800

    9.28

--- kindergarten-commons/kindergarten-common/src/main/java/com/zc/kindergarten/common/handler/GlobalExceptionHandler.java
+++ kindergarten-commons/kindergarten-common/src/main/java/com/zc/kindergarten/common/handler/GlobalExceptionHandler.java
@@ -31,146 +31,146 @@ import java.util.Map;
 @Slf4j
 public class GlobalExceptionHandler {
 
-    /**
-     * Http请求时，参数异常
-     *
-     * @param
-     * @param e
-     */
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    @ResponseBody
-    public ResponseEntity handlerRequstParams(MethodArgumentTypeMismatchException e, HttpServletRequest request) {
-        log.error("\n发生的异常：方法参数类型绑定异常，异常原因：" + e.getMessage(), e);
-        return new ResponseEntity(SystemErrors.METHOD_ARGUMENT_TYPE_MISMATCH_EXCEPTION);
-    }
+	/**
+	 * Http请求时，参数异常
+	 *
+	 * @param
+	 * @param e
+	 */
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	@ResponseBody
+	public ResponseEntity handlerRequstParams(MethodArgumentTypeMismatchException e, HttpServletRequest request) {
+		log.error("\n发生的异常：方法参数类型绑定异常", e);
+		return new ResponseEntity(SystemErrors.METHOD_ARGUMENT_TYPE_MISMATCH_EXCEPTION);
+	}
 
-    /**
-     * Http请求时，参数异常
-     *
-     * @param
-     * @param e
-     */
-    @ExceptionHandler(ServletException.class)
-    @ResponseBody
-    public ResponseEntity ServletException(ServletException e, HttpServletRequest request) {
-        log.error("\n发生的异常：请求处理异常，异常原因：" + e.getMessage(), e);
-        return new ResponseEntity(SystemErrors.SERVLET_EXCEPTION);
-    }
+	/**
+	 * Http请求时，参数异常
+	 *
+	 * @param
+	 * @param e
+	 */
+	@ExceptionHandler(ServletException.class)
+	@ResponseBody
+	public ResponseEntity ServletException(ServletException e, HttpServletRequest request) {
+		log.error("\n发生的异常：请求处理异常", e);
+		return new ResponseEntity(SystemErrors.SERVLET_EXCEPTION);
+	}
 
-    /**
-     * 服务器内部出错
-     *
-     * @param
-     * @param e
-     */
-    @ExceptionHandler(SysException.class)
-    @ResponseBody
-    public ResponseEntity SysException(SysException e, HttpServletRequest request) {
-        log.error("\n发生的异常：自定义系统异常，异常原因：" + e.getError().getMessage() + (e.getData() == null ? "" : (",data=" + e.getData())), e);
-        return new ResponseEntity(e.getError().getCode(), e.getError().getMessage());
-    }
+	/**
+	 * 服务器内部出错
+	 *
+	 * @param
+	 * @param e
+	 */
+	@ExceptionHandler(SysException.class)
+	@ResponseBody
+	public ResponseEntity SysException(SysException e, HttpServletRequest request) {
+		log.error("\n发生的异常：自定义系统异常", e);
+		return new ResponseEntity(e.getError().getCode(), e.getError().getMessage());
+	}
 
-    /**
-     * 请求超时
-     *
-     * @param
-     * @param e
-     */
-    @ExceptionHandler(RequestTimeOutException.class)
-    @ResponseBody
-    public ResponseEntity RequestTimeOutException(RequestTimeOutException e, HttpServletRequest request) {
-        log.error("\n发生的异常：请求超时，异常原因：" + e.getError().getMessage() + (e.getData() == null ? "" : (",data=" + e.getData())), e);
-        return new ResponseEntity(e.getError().getCode(), e.getError().getMessage());
-    }
+	/**
+	 * 请求超时
+	 *
+	 * @param
+	 * @param e
+	 */
+	@ExceptionHandler(RequestTimeOutException.class)
+	@ResponseBody
+	public ResponseEntity RequestTimeOutException(RequestTimeOutException e, HttpServletRequest request) {
+		log.error("\n发生的异常：请求超时，异常原因：" + e.getError().getMessage() + (e.getData() == null ? "" : (",data=" + e.getData())), e);
+		return new ResponseEntity(e.getError().getCode(), e.getError().getMessage());
+	}
 
-    /**
-     * 参数出错
-     *
-     * @param
-     * @param e
-     */
-    @ExceptionHandler(ParamsException.class)
-    @ResponseBody
-    public ResponseEntity ParamsException(ParamsException e, HttpServletRequest request) {
-        log.error("\n发生的异常：参数错误，异常原因：" + e.getError().getMessage() + (e.getData() == null ? "" : (",data=" + e.getData())), e);
-        return new ResponseEntity(e.getError().getCode(), e.getError().getMessage());
-    }
+	/**
+	 * 参数出错
+	 *
+	 * @param
+	 * @param e
+	 */
+	@ExceptionHandler(ParamsException.class)
+	@ResponseBody
+	public ResponseEntity ParamsException(ParamsException e, HttpServletRequest request) {
+		log.error("\n发生的异常：参数错误", e);
+		return new ResponseEntity(e.getError().getCode(), e.getError().getMessage());
+	}
 
 
-    /**
-     * 基础异常处理
-     *
-     * @param request
-     * @param e
-     * @return
-     */
-    @ExceptionHandler(BaseException.class)
-    @ResponseBody
-    public ResponseEntity BaseException(HttpServletRequest request, BaseException e) {
-        log.error("\n发生的异常：BaseException，异常原因：" + e.getError().getMessage() + (e.getData() == null ? "" : (",data=" + e.getData())), e);
-        return new ResponseEntity(e.getError().getCode(), e.getError().getMessage());
-    }
+	/**
+	 * 基础异常处理
+	 *
+	 * @param request
+	 * @param e
+	 * @return
+	 */
+	@ExceptionHandler(BaseException.class)
+	@ResponseBody
+	public ResponseEntity BaseException(HttpServletRequest request, BaseException e) {
+		log.error("\n发生的异常：BaseException", e);
+		return new ResponseEntity(e.getError().getCode(), e.getError().getMessage());
+	}
 
-    /**
-     * 统一未知异常处理
-     *
-     * @param request
-     * @param e
-     * @return
-     */
-    @ExceptionHandler(Exception.class)
-    @ResponseBody
-    public ResponseEntity handleGlobal(HttpServletRequest request, Exception e) {
-        log.error("\n发生的异常：Exception，异常原因：" + e.getMessage(), e);
-        return new ResponseEntity(SystemErrors.SYSTEM_ERROR);
-    }
+	/**
+	 * 统一未知异常处理
+	 *
+	 * @param request
+	 * @param e
+	 * @return
+	 */
+	@ExceptionHandler(Exception.class)
+	@ResponseBody
+	public ResponseEntity handleGlobal(HttpServletRequest request, Exception e) {
+		log.error("\n发生的异常：Exception", e);
+		return new ResponseEntity(SystemErrors.SYSTEM_ERROR);
+	}
 
-    /**
-     * 请求参数格式异常处理
-     *
-     * @param exception
-     * @return
-     * @throws Exception
-     */
-    @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    @ResponseBody
-    public ResponseEntity MethodArgumentNotValidHandler(MethodArgumentNotValidException exception, HttpServletRequest request) throws Exception {
-        BindingResult result = exception.getBindingResult();
-        log.error("\n发生的异常：请求参数格式异常，异常原因：" + exception.getMessage(), exception);
-        return bindParamValid(result);
-    }
+	/**
+	 * 请求参数格式异常处理
+	 *
+	 * @param exception
+	 * @return
+	 * @throws Exception
+	 */
+	@ExceptionHandler(value = MethodArgumentNotValidException.class)
+	@ResponseBody
+	public ResponseEntity MethodArgumentNotValidHandler(MethodArgumentNotValidException exception, HttpServletRequest request) throws Exception {
+		BindingResult result = exception.getBindingResult();
+		log.error("\n发生的异常：请求参数格式异常", exception);
+		return bindParamValid(result);
+	}
 
-    /**
-     * 请求参数格式异常处理
-     *
-     * @param exception
-     * @return
-     * @throws Exception
-     */
-    @ExceptionHandler(value = BindException.class)
-    @ResponseBody
-    public ResponseEntity BindException(BindException exception, HttpServletRequest request) throws Exception {
-        BindingResult result = exception.getBindingResult();
-        log.error("\n发生的异常：请求参数绑定异常，异常原因：" + exception.getMessage(), exception);
-        return bindParamValid(result);
-    }
+	/**
+	 * 请求参数格式异常处理
+	 *
+	 * @param exception
+	 * @return
+	 * @throws Exception
+	 */
+	@ExceptionHandler(value = BindException.class)
+	@ResponseBody
+	public ResponseEntity BindException(BindException exception, HttpServletRequest request) throws Exception {
+		BindingResult result = exception.getBindingResult();
+		log.error("\n发生的异常：请求参数绑定异常", exception);
+		return bindParamValid(result);
+	}
 
-    /**
-     * 数据绑定参数校验
-     *
-     * @param result
-     * @return
-     */
-    private ResponseEntity bindParamValid(BindingResult result) {
-        List<Object> invalidArguments = new ArrayList<>();
-        if (result.hasErrors()) {
-            for (FieldError error : result.getFieldErrors()) {
-                Map<String, String> map = new HashMap<>();
-                map.put("filed", error.getField());
-                map.put("errorMsg", error.getDefaultMessage());
-                invalidArguments.add(map);
-            }
-        }
-        return new ResponseEntity(SystemErrors.REQUEST_PARAM_ERROR);
-    }
+	/**
+	 * 数据绑定参数校验
+	 *
+	 * @param result
+	 * @return
+	 */
+	private ResponseEntity bindParamValid(BindingResult result) {
+		List<Object> invalidArguments = new ArrayList<>();
+		if (result.hasErrors()) {
+			for (FieldError error : result.getFieldErrors()) {
+				Map<String, String> map = new HashMap<>();
+				map.put("filed", error.getField());
+				map.put("errorMsg", error.getDefaultMessage());
+				invalidArguments.add(map);
+			}
+		}
+		return new ResponseEntity(SystemErrors.REQUEST_PARAM_ERROR);
+	}
 }
